8fd6908ab28b25387d30ee4a1a5e51fe7cac6938
Mohammad Rahmani <rto1680@gmail.com>
2023-10-21 19:39:16 +0330

    Update project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:day12_login/Animation/FadeAnimation.dart';
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 
 void main() => runApp(
   MaterialApp(
@@ -31,7 +31,7 @@ class HomePage extends StatelessWidget {
 	                    left: 30,
 	                    width: 80,
 	                    height: 200,
-	                    child: FadeAnimation(1, Container(
+	                    child: FadeInUp(duration: Duration(seconds: 1), child: Container(
 	                      decoration: BoxDecoration(
 	                        image: DecorationImage(
 	                          image: AssetImage('assets/images/light-1.png')
@@ -43,7 +43,7 @@ class HomePage extends StatelessWidget {
 	                    left: 140,
 	                    width: 80,
 	                    height: 150,
-	                    child: FadeAnimation(1.3, Container(
+	                    child: FadeInUp(duration: Duration(milliseconds: 1200), child: Container(
 	                      decoration: BoxDecoration(
 	                        image: DecorationImage(
 	                          image: AssetImage('assets/images/light-2.png')
@@ -56,7 +56,7 @@ class HomePage extends StatelessWidget {
 	                    top: 40,
 	                    width: 80,
 	                    height: 150,
-	                    child: FadeAnimation(1.5, Container(
+	                    child: FadeInUp(duration: Duration(milliseconds: 1300), child: Container(
 	                      decoration: BoxDecoration(
 	                        image: DecorationImage(
 	                          image: AssetImage('assets/images/clock.png')
@@ -65,7 +65,7 @@ class HomePage extends StatelessWidget {
 	                    )),
 	                  ),
 	                  Positioned(
-	                    child: FadeAnimation(1.6, Container(
+	                    child: FadeInUp(duration: Duration(milliseconds: 1600), child: Container(
 	                      margin: EdgeInsets.only(top: 50),
 	                      child: Center(
 	                        child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
@@ -79,11 +79,12 @@ class HomePage extends StatelessWidget {
 	              padding: EdgeInsets.all(30.0),
 	              child: Column(
 	                children: <Widget>[
-	                  FadeAnimation(1.8, Container(
+	                  FadeInUp(duration: Duration(milliseconds: 1800), child: Container(
 	                    padding: EdgeInsets.all(5),
 	                    decoration: BoxDecoration(
 	                      color: Colors.white,
 	                      borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Color.fromRGBO(143, 148, 251, 1)),
 	                      boxShadow: [
 	                        BoxShadow(
 	                          color: Color.fromRGBO(143, 148, 251, .2),
@@ -97,23 +98,24 @@ class HomePage extends StatelessWidget {
 	                        Container(
 	                          padding: EdgeInsets.all(8.0),
 	                          decoration: BoxDecoration(
-	                            border: Border(bottom: BorderSide(color: Colors.grey[100]))
+	                            border: Border(bottom: BorderSide(color:  Color.fromRGBO(143, 148, 251, 1)))
 	                          ),
 	                          child: TextField(
 	                            decoration: InputDecoration(
 	                              border: InputBorder.none,
 	                              hintText: "Email or Phone number",
-	                              hintStyle: TextStyle(color: Colors.grey[400])
+	                              hintStyle: TextStyle(color: Colors.grey[700])
 	                            ),
 	                          ),
 	                        ),
 	                        Container(
 	                          padding: EdgeInsets.all(8.0),
 	                          child: TextField(
+                              obscureText: true,
 	                            decoration: InputDecoration(
 	                              border: InputBorder.none,
 	                              hintText: "Password",
-	                              hintStyle: TextStyle(color: Colors.grey[400])
+	                              hintStyle: TextStyle(color: Colors.grey[700])
 	                            ),
 	                          ),
 	                        )
@@ -121,7 +123,7 @@ class HomePage extends StatelessWidget {
 	                    ),
 	                  )),
 	                  SizedBox(height: 30,),
-	                  FadeAnimation(2, Container(
+	                  FadeInUp(duration: Duration(milliseconds: 1900), child: Container(
 	                    height: 50,
 	                    decoration: BoxDecoration(
 	                      borderRadius: BorderRadius.circular(10),
@@ -137,7 +139,7 @@ class HomePage extends StatelessWidget {
 	                    ),
 	                  )),
 	                  SizedBox(height: 70,),
-	                  FadeAnimation(1.5, Text("Forgot Password?", style: TextStyle(color: Color.fromRGBO(143, 148, 251, 1)),)),
+	                  FadeInUp(duration: Duration(milliseconds: 2000), child: Text("Forgot Password?", style: TextStyle(color: Color.fromRGBO(143, 148, 251, 1)),)),
 	                ],
 	              ),
 	            )
